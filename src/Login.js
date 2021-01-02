c1d864d1e3dc8d14b6f33badf69c061d9f210e56
shuvojyoti <shuvojyoti.golder@gmail.com>
2021-01-02 11:47:00 +0600

    login page added

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,32 @@ import React from "react";
 import "./Login.css";
 
 function Login() {
-  return <div className="login"></div>;
+  const loginToapp = () => {};
+  const register = () => {};
+
+  return (
+    <div className="login">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/LinkedIn_Logo_2013.svg/1024px-LinkedIn_Logo_2013.svg.png"
+        alt=""
+      />
+      <form>
+        <input placeholder="Full name (required if registering)" type="text" />
+        <input placeholder="Profile pic URL (optional)" type="text" />
+        <input placeholder="Email" type="email" />
+        <input placeholder="Password" type="password" />
+        <button type="submit" onClick={loginToapp}>
+          Sign in
+        </button>
+        <p>
+          Not a member?
+          <span className="login_register" onClick={register}>
+            Register Now
+          </span>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
